e0651e7c1714d3b2548a99e72aaa9bf5edec2d37
flamboh <oliver.boorstein@gmail.com>
2025-09-22 15:49:37 -0700

    button hover updated, background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="font-sans min-h-screen flex items-center justify-center"></div>
+    <div className="font-sans min-h-screen flex items-center justify-center bg-background">
+      <Button>Click me</Button>
+    </div>
   );
 }
